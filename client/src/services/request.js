8f87c403556c57b00d65af34bd6609985c696895
BaptisteFredj <redj2304@gmail.com>
2024-08-30 09:41:35 +0200

    removed dead code

--- client/src/services/request.js
+++ client/src/services/request.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 export default function getArtistList() {
-  return (
-    axios
-      // .get("http://localhost:3310/api/artists")
-      .get(`${import.meta.env.VITE_API_URL}/api/artists`)
-      .then((response) => response.data)
-      .catch((error) => console.error(error))
-  );
+  return axios
+    .get(`${import.meta.env.VITE_API_URL}/api/artists`)
+    .then((response) => response.data)
+    .catch((error) => console.error(error));
 }
