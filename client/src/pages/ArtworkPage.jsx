1a9132b236c33f07c7031c05f8b23a0a63accdab
DANIEL WONG PIN <daniel.wongpin@gmail.com>
2024-08-30 12:23:33 +0200

    feat : creation ArtworkPage.jsx with back and front and file artworkpage.css

--- client/src/pages/ArtworkPage.jsx
+++ client/src/pages/ArtworkPage.jsx
@@ -6,44 +6,47 @@ function ArtworkPage() {
   const { artwork } = useLoaderData();
   return (
     <>
-      <div className="chemin">
+      <div className="artworkPage_navigate">
         <img
-          className="chemin_img"
+          className="artworkPage_chemin_img"
           src="../src/assets/images/maison.png"
           alt="maison"
         />
-        <h1 className="suivi_page"> {"> Les oeuvres > Nom de l'oeuvre"}</h1>
+        <h1 className="artworkPage_suivi_page">
+          {" "}
+          {"> Les oeuvres > Nom de l'oeuvre"}
+        </h1>
       </div>
 
-      <div className="tag">
+      <div className="artworkPage_tag">
         <p>Tags :</p>
-        <ul className="tags">
+        <ul className="artworkPage_tags">
           <li>tag 1</li>
           <li>tag 2</li>
           <li>tag 3</li>
         </ul>
       </div>
-      <div className="one_oeuvre">
+      <div className="artworkPage_one_oeuvre">
         <img
-          className="one_oeuvre_pic"
-          src="../src/assets/images/ImageTest1.jpg"
+          className="artworkPage_one_oeuvre_pic"
+          src={artwork.image_url}
           alt="chaton"
         />
       </div>
-      <div className="name_oeuvre">
+      <div className="artworkPage_name_oeuvre">
         <p>Léo DUMONT</p>
         <p>{artwork.title}</p>
         <p>{artwork.date}</p>
       </div>
 
-      <div className="fav_com">
+      <div className="artworkPage_fav_com">
         <p>Un coup de coeur ? Ajouter la à vos favoris</p>
         <p>Une pensée ? Faites la vivre en commentaire</p>
       </div>
 
-      <div className="tech_oeuvre">
+      <div className="artworkPage_tech_oeuvre">
         <p>A propos de l'oeuvre</p>
-        <ul className="divers_tech">
+        <ul className="artworkPage_divers_tech">
           <li>
             <span>Technique :</span> non renseigné
           </li>
