ae47c0d79ba62d95f18a4208366fa03f3dbddb79
BaptisteFredj <redj2304@gmail.com>
2024-08-30 09:40:38 +0200

    add vite api url

--- client/src/services/request.js
+++ client/src/services/request.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 export default function getArtistList() {
-  return axios
-    .get("http://localhost:3310/api/artists")
-    .then((response) => response.data)
-    .catch((error) => console.error(error));
+  return (
+    axios
+      // .get("http://localhost:3310/api/artists")
+      .get(`${import.meta.env.VITE_API_URL}/api/artists`)
+      .then((response) => response.data)
+      .catch((error) => console.error(error))
+  );
 }
