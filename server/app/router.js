11ee8dfbdd8b4c3038199fcbcff62a36d7223c40
BaptisteFredj <redj2304@gmail.com>
2024-08-30 10:38:05 +0200

    add post to artist and artwork actions

--- server/app/router.js
+++ server/app/router.js
@@ -8,8 +8,10 @@ const artworks = require("./controllers/artworkActions");
 
 router.get("/artists", artists.browse);
 router.get("/artists/:id", artists.read);
+router.post("/artists", artists.add);
 
 router.get("/artworks", artworks.browse);
 router.get("/artworks/:id", artworks.read);
+router.post("/artworks", artworks.add);
 
 module.exports = router;
