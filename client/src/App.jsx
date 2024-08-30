e4f7ad01345e92657f1461a4d20173217ed6543b
Monica Pardo <moonyruna3@gmail.com>
2024-08-30 11:34:01 +0200

    Delete unnecesary files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 
 import "./App.css";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <>
-      <Navbar />
       <Homepage />
       <main>
         <Outlet />
