e14d5862959e45c595057183b54ce776c52744cc
clement Vigouroux <clement.vigouroux@gmail.com>
2024-08-30 11:58:57 +0200

    change path and config

--- server/database/models/ArtworkRepository.js
+++ server/database/models/ArtworkRepository.js
@@ -12,8 +12,7 @@ class ArtworkRepository extends AbstractRepository {
 
   async read(id) {
     const [rows] = await this.database.query(
-      `
-      select artwork.*, artist.pseudo artiste from ${this.table}  INNER JOIN artist on artwork.artist_id = artist.id WHERE artwork.id = ? `,
+      `select artwork.*, artist.pseudo artiste from ${this.table}  INNER JOIN artist on artwork.artist_id = artist.id WHERE artwork.id = ? `,
       [id]
     );
     return rows[0];
