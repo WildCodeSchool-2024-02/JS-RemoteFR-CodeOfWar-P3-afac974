eb49bfd456c95e451404c6b2e4cd7a0fdcb57e1b
Monica Pardo <moonyruna3@gmail.com>
2024-08-31 16:52:36 +0200

    npx prettier . --write

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,12 @@
 import { Outlet } from "react-router-dom";
 
 import "./App.css";
-// import Navbar from "./components/Navbar";
-import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <>
-      <Homepage />
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <main>
+      <Outlet />
+    </main>
   );
 }
 
