65f2145ca89711d40c36579528e6a5cd963efdbf
Kevin Peset <97118056+kpeset@users.noreply.github.com>
2024-09-10 16:04:30 +0200

    Update Homepage.jsx

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,9 +1,5 @@
 import { useLoaderData, Link } from "react-router-dom";
 
-import Navbar from "../components/Navbar";
-
-import "../assets/styles/homepage.css";
-
 import ArtworkComponent from "../components/ArtworkComponent";
 import "../assets/styles/homepage.css";
 
