2862c556cdfd6cad36050a03d456ad8267a34071
Kevin Peset <kpeset@Air-de-Kevin.fritz.box>
2024-09-17 13:01:29 +0200

    Remplacement du type d'import

--- server/app/services/middleware.js
+++ server/app/services/middleware.js
@@ -1,26 +1,23 @@
-import multer from "multer";
-import path from "path";
-import { v4 as uuidv4 } from 'uuid';
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
 
-
-
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "./public/uploads")
+    cb(null, "./public/uploads");
   },
   filename(req, file, cb) {
     const id = uuidv4();
     console.info(id);
     const pictureName = `${id}${path.extname(file.originalname)}`;
     req.body.image_url = pictureName;
-    cb(null, pictureName)
-  }
-})
+    cb(null, pictureName);
+  },
+});
 const uploadImg = (req, res, next) => {
-
   const upload = multer({ storage });
   return upload.single("image")(req, res, next);
-}
+};
 
 export default uploadImg;
