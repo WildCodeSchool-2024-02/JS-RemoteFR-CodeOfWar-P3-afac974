950608d10287753b5b857bed85efef10fb4e1473
DANIEL WONG PIN <daniel.wongpin@gmail.com>
2024-09-22 03:55:11 +0200

    creation artistlist.css

--- client/src/components/ArtistComponent.jsx
+++ client/src/components/ArtistComponent.jsx
@@ -2,20 +2,28 @@ import PropTypes from "prop-types";
 
 export default function ArtistComponent({ artist }) {
   return (
-    <ul>
-      <li>
+    <div className="artistlist_card">
+      <img
+        src={artist.portrait}
+        alt={`${artist.firstname} ${artist.lastname}`}
+        className="artistlist_image"
+      />
+      <div className="artistlist_name">
         {artist.firstname} {artist.lastname}
-      </li>
-      <li>Nationalité : {artist.nationality}</li>
-      <li>Biographie : {artist.biography}</li>
-    </ul>
+      </div>
+      <div className="artistlist_info">Nationalité : {artist.nationality}</div>
+      <div className="artistlist_info">Biographie : {artist.biography}</div>
+    </div>
   );
 }
 
 ArtistComponent.propTypes = {
-  id: PropTypes.number.isRequired,
-  firstname: PropTypes.string.isRequired,
-  lastname: PropTypes.string.isRequired,
-  nationality: PropTypes.string.isRequired,
-  biography: PropTypes.string.isRequired,
-}.isRequired;
+  artist: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    firstname: PropTypes.string.isRequired,
+    lastname: PropTypes.string.isRequired,
+    nationality: PropTypes.string.isRequired,
+    biography: PropTypes.string.isRequired,
+    portrait: PropTypes.string.isRequired,
+  }).isRequired,
+};
