23f0be0f058abe593108ad3b5c487323af5161b7
Monica Pardo <moonyruna3@gmail.com>
2024-09-25 11:17:36 +0200

    delete component not used

--- client/src/pages/MyArtworks.jsx
+++
@@ -1,9 +0,0 @@
-import { useLoaderData } from "react-router-dom";
-
-function MyArtworks() {
-  const artworks = useLoaderData();
-  console.info(artworks);
-  return <div>Hello from MyArtworks</div>;
-}
-
-export default MyArtworks;
