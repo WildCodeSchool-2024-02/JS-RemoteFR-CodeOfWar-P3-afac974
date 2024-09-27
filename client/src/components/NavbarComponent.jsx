f3f2e8d66e4a599f6195189e54ca3a441b5200ce
Damien / Damphe <goguet.damien@hotmail.fr>
2024-09-27 10:57:32 +0200

    Feat: add burgerMenu

--- client/src/components/NavbarComponent.jsx
+++ client/src/components/NavbarComponent.jsx
@@ -2,25 +2,29 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getUserId } from "../services/request";
 import { useAuth } from "../context/AuthContext";
-
 import IconsComponent from "./IconsComponent";
 
 function Navbar() {
   const { auth } = useAuth();
-  const [userId, setUserId] = useState(null);
+  const [userData, setUserData] = useState({ id: null, isAdmin: false });
 
   useEffect(() => {
-    const fetchUserId = async () => {
+    const fetchUserData = async () => {
       if (auth) {
-        const id = await getUserId();
-        setUserId(id);
+        const data = await getUserId();
+        setUserData({ id: data.id, isAdmin: data.is_admin });
       } else {
-        setUserId(null);
+        setUserData({ id: null, isAdmin: false });
       }
     };
 
-    fetchUserId();
+    fetchUserData();
   }, [auth]);
+
+  const [showLinks, setShowLinks] = useState(false);
+  const handleShowLinks = () => {
+    setShowLinks(!showLinks);
+  };
 
   return (
     <nav className="navbarcomponent_navArea">
@@ -33,22 +37,22 @@ function Navbar() {
       </Link>
 
       <ul className="navbarcomponent_list">
-        {/* ------------------------------AUTHENTIFICATION/DASHBOARD----------------------------------- */}
+        {/* ---------------------------Authentification/Dashboard--------------------------- */}
         <li>
           <Link
-            to={userId ? "/dashboard" : "/authentification"}
+            to={userData.id ? "/dashboard" : "/authentification"}
             className="navBar_userButton"
           >
             <IconsComponent
               className="navbarcomponent_user_icon"
-              src={userId ? "userConnectedIcon" : "userIcon"}
+              src={userData.id ? "userConnectedIcon" : "userIcon"}
             />
           </Link>
         </li>
-        {/* ------------------------------FAVORIS----------------------------------- */}
+        {/* ---------------------------Favoris--------------------------- */}
         <li>
-          {userId ? (
-            <Link to={`/favoris/${userId}`}>
+          {userData.id ? (
+            <Link to={`/favoris/${userData.id}`}>
               <IconsComponent
                 className="navbarcomponent_favorite_icon"
                 alt="heart icon for favourites artworks"
@@ -65,14 +69,74 @@ function Navbar() {
             </Link>
           )}
         </li>
-        {/* ------------------------------MENU----------------------------------- */}
-        <li>
-          <IconsComponent
-            className="navbarcomponent_menu_icon"
-            alt="burger menu icon"
-            src="menuburger"
-          />
-        </li>
+        {/* ---------------------------Menu---------------------------------- */}
+        <div className="navbarcomponent_burgerMenu">
+          <nav
+            className={`navBarComponent_navContainer ${showLinks ? "show-nav" : "hide-nav"}`}
+          >
+            <div className="navBarComponent_listLinks">
+              <button
+                className="navBarComponent_menuButton"
+                type="button"
+                aria-label="MenuBurger"
+                onClick={handleShowLinks}
+              >
+                <span className="navBarComponent_burgerBars" />
+              </button>
+              <Link
+                to="/artists"
+                className="navBarComponent_navLinks"
+                onClick={showLinks}
+              >
+                <li className="navBarComponent_1"> Tous les artistes</li>
+              </Link>
+              <Link
+                to="/artworks"
+                className="navBarComponent_navLinks"
+                onClick={showLinks}
+              >
+                <li className="navBarComponent_2"> Toutes les oeuvres</li>
+              </Link>
+              <Link
+                to="/exhibition"
+                className="navBarComponent_navLinks "
+                onClick={showLinks}
+              >
+                <li className="navBarComponent_3">
+                  Les expositions temporaire
+                </li>
+              </Link>
+              <Link
+                to="/exhibition"
+                className="navBarComponent_navLinks"
+                onClick={showLinks}
+              >
+                <li className="navBarComponent_4">
+                  Les expositions permanente
+                </li>
+              </Link>
+              <Link
+                to="/"
+                className="navBarComponent_navLinks"
+                onClick={showLinks}
+              >
+                <li className="navBarComponent_5"> A propos de nous</li>
+              </Link>
+
+              {userData.isAdmin ? (
+                <Link
+                  to="/exhibitionForm"
+                  onClick={showLinks}
+                  className="navBarComponent_navLinks"
+                >
+                  <li className="navBarComponent_6">Formulaire exposition</li>
+                </Link>
+              ) : (
+                ""
+              )}
+            </div>
+          </nav>
+        </div>
       </ul>
     </nav>
   );
