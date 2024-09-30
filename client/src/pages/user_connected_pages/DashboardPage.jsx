2586e227c904fe441b97e71e26317584e5492ed1
clement Vigouroux <clement.vigouroux@gmail.com>
2024-09-30 16:26:45 +0200

    fix navbar component

--- client/src/pages/user_connected_pages/DashboardPage.jsx
+++ client/src/pages/user_connected_pages/DashboardPage.jsx
@@ -1,31 +1,54 @@
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 import IconsComponent from "../../components/IconsComponent";
 
-function DashboardPage() {
+function DashboardPage() {  const { setAuth } = useAuth();
+const navigate = useNavigate();
+
+  const handleDisconnected = () => {
+    setAuth(null);
+    navigate("/authentification");
+
+  };
+  
   return (
-    <div className="auth_navlist">
-      <li className="auth_navOnline">
-        <Link className="auth_navLinksOnline" to="/add">
-          <IconsComponent
-            className="userpage_icon"
-            src="importicon"
-            alt="upload icon"
-          />
-        </Link>
-      </li>
-      <li className="auth_navOnline">
-        <Link to="/artwork_dashboard" className="auth_navLinksOnline">
-          Mes publications
-        </Link>
-      </li>
-      <li className="auth_navOnline">
-        <Link to="/myinformations" className="auth_navLinksOnline">
-          Mes informations personnel
-        </Link>
-      </li>
-    </div>
+           <div className="auth_navlist">
+            <h1>DASHBOARDPAGE</h1>
+          <li className="auth_navOnline">
+            <Link to="/" className="auth_navLinksOnline">
+              Mon profil
+            </Link>
+          </li>
+          <li className="auth_navOnline">
+            <Link className="auth_navLinksOnline" to="/add">
+              <IconsComponent
+                className="userpage_icon"
+                src="importicon"
+                alt="upload icon"
+              />
+            </Link>
+          </li>
+
+          <li className="auth_navOnline">
+            <Link to="/artwork_dashboard" className="auth_navLinksOnline">
+              Mes publications
+            </Link>
+          </li>
+          <li className="auth_navOnline">
+            <Link to="/myinformations" className="auth_navLinksOnline">
+              Mes informations personnel
+            </Link>
+          </li>
+          <li className="auth_navOnline">
+            <button
+              className="auth_navLinksOnline auth_disconnect"
+              type="button"
+              onClick={handleDisconnected}
+            >
+              Déconnexion
+            </button>
+          </li> 
+      </div>
   );
 }
-
 export default DashboardPage;
